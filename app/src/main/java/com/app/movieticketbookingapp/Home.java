25d463592e894984e4a5f5fd69b6670f9b394c9d
ThamanGurung01 <tgurung519@gmail.com>
2025-05-30 19:15:43 +0545

    intent and home page connections for testing

--- app/src/main/java/com/app/movieticketbookingapp/Home.java
+++ app/src/main/java/com/app/movieticketbookingapp/Home.java
@@ -1,16 +1,49 @@
 package com.app.movieticketbookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
-
+    Button login;
+    Button signup;
+    Button dashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        login=findViewById(R.id.btnLoginPage);
+        signup=findViewById(R.id.btnSignupPage);
+        dashboard=findViewById(R.id.btnDashboardPage);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, login.class);
+                startActivity(intent);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, signup.class);
+                startActivity(intent);
+            }
+        });
+        dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
